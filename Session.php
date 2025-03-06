dd23b066559e278003ceb091afcb7abeb0449dfa
melaren12 <melaren6@gmail.com>
2025-03-06 20:26:50 +0400

    aa

--- Session.php
+++ Session.php
@@ -16,7 +16,7 @@ class SessionManager
 
     public static function changeDir(string $dir): string
     {
-        $new_path = realpath($_SESSION['current_dir'] . '//' . $dir);
+        $new_path = realpath($_SESSION['current_dir'] . '/' . $dir);
         if ($new_path && is_dir($new_path)) {
             $_SESSION['current_dir'] = $new_path;
             return "Directory changed to: " . $_SESSION['current_dir'];
