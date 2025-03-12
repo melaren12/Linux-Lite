e7708dc653915c9d9d2c7b5510f323bf728919fd
melaren12 <melaren6@gmail.com>
2025-03-12 14:35:59 +0400

    moving to phpStorm

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -17,14 +17,4 @@ class SessionManager
         return $_SESSION['current_dir'];
     }
 
-    public static function changeDir(string $dir): string
-    {
-        $new_path = realpath($_SESSION['current_dir'] . '/' . $dir);
-        if ($new_path && is_dir($new_path)) {
-            $_SESSION['current_dir'] = $new_path;
-           
-            return "Directory changed to: " . $_SESSION['current_dir'];
-        }
-        return "Directory not found";
-    }
 }
